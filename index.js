a1fd738821eb73b363949af4740e77bd91b5859a
jasn4real <tobeobi@pursuit.org>
2023-04-02 16:36:53 -0400

    Tree Lab Completed

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    let checkNodes = [this.root];
+
+    while (checkNodes.length > 0) {
+      //initialized the checkNodes with the root node of the tree, which then enters a while loop that runs as long as there are still nodes in the checkNodes.
+
+      let current = checkNodes.shift();  //Assigned the first node the variable "current", then checks if it's equal to the data argument
+      if (current.data === data) {
+        return current;
+      }
+      checkNodes.push(...current.children);
+    }
+    return null;  //returns null if the node doesn't exist
+  }
 }
 
 module.exports = { TreeNode, Tree };
