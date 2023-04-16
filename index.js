66b7223d6e3ae13ec0c5fd85c0c1452d518baf34
Brandon Bennett <brandonbennett@Pursuits-Air.lan>
2023-04-16 17:12:40 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,16 +1,75 @@
 class TreeNode {
   constructor(data = null) {
     this.data = data;
-    this.children = [];
+    this.children = []
+  
   }
 }
 
 class Tree {
   constructor(node) {
-    this.root = node;
+    this.root = node
   }
 
-  findNode(data) {}
+findNode(data, currentNode = this.root) {
+if (currentNode === null) {
+  return null
+}
+else if(currentNode.data === data){
+  return currentNode
+}
+else {
+  let arr = currentNode.children
+for (let child of arr ) {
+  let found = this.findNode(data, child)
+ if (found !== null) {
+  return found
+ }
+}
+return null
 }
 
+}
+
+}
+
+// class Tree {
+//   constructor(node) {
+//     this.root = node;
+//   }
+
+//   findNode(data) {
+//     let checkNodes = [this.root]
+  
+//   while (checkNodes[0]) {
+//     if (data === checkNodes[0].data){
+//       return checkNodes[0]
+//     }
+//     else {
+//       for (let i of checkNodes[0].children) {
+//         checkNodes.push(i)
+//       } 
+//       checkNodes.shift();
+//     }
+    
+//   }
+//   return null 
+//   }
+// }
+
 module.exports = { TreeNode, Tree };
+
+// let currentNode = this.root;
+  
+//   while(currentNode) {
+//     if(data === currentNode.value) {
+//       return currentNode.value
+//     }
+//     else if (data < currentNode.value) {
+//       currentNode = currentNode.left
+//     }
+//     else {
+//       currentNode = currentNode.right;
+//     }
+//   }
+  
